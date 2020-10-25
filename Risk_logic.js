ee5551354430968e1e8018100e31c5753a6cea88
Ben Clifford <ben.w.clifford@gmail.com>
2020-10-25 03:07:29 -0700

    Got random country placement working. Display player colors.
    - Made new setting for whether to place armies randomly.
    - Still need to resolve 2 issues with coloring army display.

--- Risk_logic.js
+++ Risk_logic.js
@@ -1,5 +1,7 @@
 /*
 The logic for my Risk emulator
+Written by Ben Clifford for personal use only
+See https://github.com/bobsbeenjamin/risk-emulator for license and disclaimers
 */
 
 /// Globals ///
@@ -10,6 +12,7 @@ var countryMap = {}; // Only used when in country location capture mode
 var drawSpace = null; // Holds the HTML canvas context
 var diceRoller = {}; // Object with data and UI elements for the dice roller modal
 var htmlCanvasElement = null; // Holds the HTML canvas element (useful for sizing)
+var initialPlacement = true; // Are we still filling the board?
 var isMuted = false; // Is the background music muted?
 var mapImage = null; // The map image that we draw on
 var mode = "startup"; // Gamestate mode: "playing" means a game is in progress
@@ -18,6 +21,7 @@ var numCountriesWithArmies = 0; // How many countries have armies on them; only 
 var numPlayers = 2; // The number of players in the game
 var playerColors = []; // The colors for each player
 var playerOrder = []; // Determined at game start; the order of play
+var randomSetup = false; // Place armies randomly at game start?
 var roundCounter = 0; // Which round we're on; a round is complete once each player takes a turn
 var song = null; // Holds the current music track
 var turnCounter = 0; // Which turn we're on; each player gets one turn per round
@@ -48,6 +52,7 @@ function setUpGameBoard(onLoad=false) {
 		diceRoller["results"] = document.getElementById("dice-roller-results");
 		diceRoller["die-1"] = document.getElementById("dice-roller-die-1");
 		diceRoller["die-2"] = document.getElementById("dice-roller-die-2");
+		diceRoller["player-info"] = document.getElementById("dice-roller-player-info");
 		diceRoller["roll-again"] = document.getElementById("dice-roller-roll-again");
 		
 		// Set up the canvas
@@ -84,27 +89,30 @@ function parseUrlParams() {
 }
 
 /**
- * Decide who goes first, place armies, and prepare for the first turn.
+ * Start a new game. Decide who goes first, place armies, and prepare for the first turn. Give the
+ * user an option to abort if a game is in progress.
  */
 function startGame() {
 	// Give the player an option to abort while a game is going
-	if(mode == "playing") {
+	if(mode == "playing" || mode == "placingArmies") {
 		startNewGame = confirm("Do you want to abandon this game and start a new game?");
 		if(!startNewGame)
 			return;
 	}
+	// Read settings
+	numPlayers = parseInt(document.getElementById("settings-num-players").value);
+	randomSetup = Boolean(document.getElementById("settings-choose-countries-randomly").value);
+	// Prepare for the first turn
+	initializeCountries();
+	initializePlayerColors();
 	// Decide who goes first
 	firstPlayer = rollDice("Decide who goes first", "goes first");
 	if(firstPlayer == 1)  // TODO: Make this more dynamic for more than 2 players
 		playerOrder = [1, 2];
 	else
 		playerOrder = [2, 1];
-	// Initial countries and player colors
-	initializeCountries();
-	initializePlayerColors();
 	// Place armies
 	firstPlacementOfArmies();
-	// Prepare for the first turn
 	// Launch first turn
 	mode = "playing";
 	mainGameLoop();
@@ -115,8 +123,8 @@ function startGame() {
  */
 function initializeCountries() {
 	for(country in countries) {
-		country.numArmies = 0;
-		country.controller = 0;
+		countries[country].numArmies = 0;
+		countries[country].controller = 0;
 	}
 }
 
@@ -136,12 +144,15 @@ function initializePlayerColors() {
  */
 function firstPlacementOfArmies() {
 	mode = "placingArmies";
-	var initialPlacement = true;
+	initialPlacement = true;
 	let startingNumberOfArmies = 50 - (5 * numPlayers);
+	startingNumberOfArmies = 21;
 	for(let i = 0; i < startingNumberOfArmies; i++) {
 		for(player of playerOrder) {
 			currentPlayer = player;
-			if(isPlayerNPC(player)) {
+			// NPCs always choose randomly (for now); also possibly choose randomly for the player,
+			// depending on the randomSetup setting
+			if(isPlayerNPC(player) || (randomSetup && initialPlacement)) {
 				placeArmy(player);
 			}
 			else {
@@ -166,7 +177,7 @@ function waitForUserAction() {
  */
 function mainGameLoop(round=1) {
 	roundCounter = round;
-	for(player in playerOrder) {
+	for(player of playerOrder) {
 		currentPlayer = player;
 		turnCounter += 1;
 		takeTurn(player);  // TODO: Implement
@@ -180,14 +191,13 @@ function mainGameLoop(round=1) {
  */
 function placeArmy(player, country=null) {
 	// For NPC, place an army. For the active player, this function is called from handleScreenClick.
-	let initialPlacement = false; // FIXME: Make this var work!
-	if(isPlayerNPC()) {
+	if(isPlayerNPC() || !country) {
 		country = getRandomCountry();
-		while(initialPlacement && country.numArmies == 0) {
+		while(initialPlacement && country.numArmies > 0) {
 			country = getRandomCountry();
 		}
 	}
-	else if(initialPlacement && country.numArmies == 0) {
+	else if(initialPlacement && country.numArmies > 0) {
 		alert("Choose a country with no armies until the world is full.");
 		return;
 	}
@@ -224,14 +234,18 @@ function getRandomCountry() {
  * Draw an overlay on the country with the number of armies, colored with the controller's color.
  */
 function drawArmiesForCountry(country) {
+	// Accept either a country key or object
+	if(typeof country == "string") {
+		country = countries[country];
+	}
 	let x = country.x - 15; // Make the country's x the center of the rectangle
 	let y = country.y + 5; // Draw the armies a little below the country name
 	let color = playerColors[country.controller];
 	// Draw a rectangle the color of the player, with the number of armies in white
-	drawSpace.fillStyle = color;
+	drawSpace.fillStyle = color;  // FIXME: First country is always black, and blue doesn't work
 	drawSpace.fillRect(x, y, 30, 20);
 	drawSpace.fillStyle = "white";
-	drawSpace.fillText(country.numArmies, 10, 50);
+	drawSpace.fillText(country.numArmies.toString(), x + 10, y + 13);
 }
 
 /**
@@ -275,7 +289,7 @@ function updateBoard() {
 	drawMap();
 	// Update the armies on each country
 	for(let country in countries) {
-		ctx.fillText(country.numArmies, country.col, country.row);
+		drawArmiesForCountry(country);
 	}
 	// popups?
 }
@@ -385,7 +399,21 @@ function rollDice(modalTitle, resultsSuffix) {
 		winner = 1;
 	else winner = 2;
 	diceRoller.results.innerText = "Player " + winner + " " + resultsSuffix;
+	diceRoller["player-info"].innerText = getPlayerColorsString();
 	return winner;
+}
+
+/**
+ * @returns A nice string with who is what color.
+ */
+function getPlayerColorsString() {
+	let returnValue = "";
+	for(let i = 0; i < numPlayers; i++) {
+		returnValue += "Player " + (i+1) + " is " + playerColors[i];
+		if(i < numPlayers - 1)
+			returnValue += " | ";
+	}
+	return returnValue;
 }
 
 /**
